22a21179b07be54efcafc77adf66f943549c761a
LDragon <1227428912@qq.com>
2020-02-03 18:03:07 +0800

    删除.idea/vcs.xml

--- git-test-project/src/com/itheima/git/FirstClass.java
+++ git-test-project/src/com/itheima/git/FirstClass.java
@@ -7,7 +7,7 @@ public class FirstClass {
         System.out.println("hello git project");
         System.out.println("hello git project");
         System.out.println("hello git project");
-        System.out.println("wahaha");
+        System.out.println("kakahaha");
     }
 
     public void test(){
